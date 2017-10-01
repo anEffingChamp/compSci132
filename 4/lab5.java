ff230e87639718c36721ad39c3a81c3193c2a54a
Jonathan Smalls <jon@smalls.rocks>
2017-09-30 21:08:04 -0400

    documentation: added chapter 5 and 6 lab assignments

---
+++ 4/lab5.java
@@ -0,0 +1,28 @@
+/**
+ * package: computer science 132
+ * lab:     5: comparing loans
+ * author:  Jonathan Smalls <jon@smalls.rocks>
+ */
+import java.util.Scanner;
+public class lab5
+{
+/**
+ * Problem Description:
+ * Write a program that lets the user enter the loan amount and loan period in number of years and displays the monthly and total payments for each interest rate starting from 5% to 8%, with an increment of 1/8. Here is a sample run:
+ *
+ * <Output>
+ * Loan Amount: 10000
+ * Number of Years: 5
+ * Interest Rate    Monthly Payment    Total Payment
+ * 5%            188.71            11322.74
+ * 5.125%        189.28            11357.13
+ * 5.25%        189.85            11391.59
+ * ...
+ * 7.875%        202.17            12129.97
+ * 8.0%            202.76            12165.83
+ * <End output>
+ */
+public static void main(String[] args)
+{
+}
+}
